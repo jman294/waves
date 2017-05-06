13a8c38601f260f202e7a01928b6b6955bc06b51
Jack DuPlessis <jduplessis294@gmail.com>
2017-05-06 16:22:54 -0400

    Add bin failsafes

--- build/build.sh
+++ build/build.sh
@@ -10,9 +10,16 @@ cp ../../src/${GAME}.love .
 
 # Make 32 and 64 bit .exe files
 cd ../win32
+if [ ! -d bin/ ]; then
+  mkdir bin/
+fi
 cat love.exe ../love/${GAME}.love > bin/${GAME}.exe
 zip -r -9 bin/${GAME}.zip -j *
+
 cd ../win64
+if [ ! -d bin/ ]; then
+  mkdir bin/
+fi
 cat love.exe ../love/${GAME}.love > bin/${GAME}.exe
 zip -r -9 bin/${GAME}.zip -j *
 
@@ -20,6 +27,9 @@ zip -r -9 bin/${GAME}.zip -j *
 GAME_IDENTIFIER="waves"
 GAME_NAME="waves"
 cd ../macos
+if [ ! -d bin/ ]; then
+  mkdir bin/
+fi
 rm -r bin/${GAME}.app
 cp -r love.app bin/${GAME}.app
 cp ../love/${GAME}.love bin/${GAME}.app/Contents/Resources/
